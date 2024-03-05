fa3219a9ae69de9d0c113dd01ef091d88cf48ee4
Sam Davis <ZKAC190@live.rhul.ac.uk>
2024-03-05 17:18:45 +0000

    Additional logical error

--- examples/carl & sam/logical-errors.py
+++ examples/carl & sam/logical-errors.py
@@ -1,5 +1,6 @@
 from hypothesis import given
 from hypothesis.strategies import text
+from hypothesis.strategies import integers
 
 # will always return one possible option but never the other, hypothesis does not check for other options since it is dynamic
 def logical_error_1(input_string):
@@ -9,6 +10,21 @@ def logical_error_1(input_string):
     else:
         return input_string + "Never returned"
     
+
+def increment(num):
+    old_num = num
+    server(old_num)
+    new_num = old_num + 1
+    return new_num
+
+def server(num):
+    print(str(num)+" sent to server")
+
+
 @given(text())
 def test_logical_error(s):
     assert logical_error_1(s) == s + "Always returned"
+
+@given(integers())
+def test_server(num):
+    assert increment(num) == num + 1
